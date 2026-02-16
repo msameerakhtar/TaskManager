cf9ed33dffb7aaaa1d67efdf67d7346a791c7838
Muhammad Sameer Akhtar <m.sameer.akhtar@techtics.ai>
2026-02-16 12:56:21 +0500

    enha: [TM-0009] implemented redux instead of session restore

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,22 +1,30 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const savedUser = JSON.parse(localStorage.getItem('user'));
+const savedToken = localStorage.getItem('userToken');
+
 const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    user: null,
-    token: localStorage.getItem('userToken') || null,
-    isAuthenticated: !!localStorage.getItem('userToken'),
+    user: savedUser || null,
+    token: savedToken || null,
+    isAuthenticated: !!savedToken,
   },
   reducers: {
     login: (state, action) => {
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isAuthenticated = true;
+
+      localStorage.setItem('user', JSON.stringify(action.payload.user));
+      localStorage.setItem('userToken', action.payload.token);
     },
     logout: (state) => {
       state.user = null;
       state.token = null;
       state.isAuthenticated = false;
+
+      localStorage.removeItem('user');
       localStorage.removeItem('userToken');
     },
   },
